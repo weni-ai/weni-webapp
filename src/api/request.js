55dc1f47f1eea23de99f5bf2f41639a122b2e6e9
Matheus Cristian <mathues@email.com>
2021-05-19 16:18:23 -0300

    feat: used oidc-client package to manage tokens

--- src/api/request.js
+++ src/api/request.js
@@ -1,13 +1,10 @@
 import axios from 'axios';
-import store from '../store';
 
 export default {
   $http(header = {}) {
     return axios.create({
       baseURL: process.env.VUE_APP_ROOT_API,
       headers: {
-        ...(store.getters.authToken()
-          ? { Authorization: `Bearer ${store.getters.authToken()}` } : {}),
         ...header,
       },
     });
