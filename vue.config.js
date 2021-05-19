0aea6451ff61f3b5e8b8fe8ac554708e1fbfb559
Matheus Cristian <mathues@email.com>
2021-05-19 16:12:06 -0300

    feat: removed luigi packages

--- vue.config.js
+++ vue.config.js
@@ -8,13 +8,6 @@ module.exports = {
   devServer: {
     disableHostCheck: true
   },
-  pages: {
-    sampleapp: {
-      entry: 'src/main.js',
-      template: 'public/sampleapp.html',
-      filename: 'sampleapp.html'
-    },
-  },
   lintOnSave:true,
   runtimeCompiler: true,
   outputDir: 'dist',
@@ -40,15 +33,9 @@ module.exports = {
     plugins: [
       new CopyWebpackPlugin([
           {context:'public',to:'index.html',from:'index.html'},
-          {context:'node_modules/@luigi-project/core',to:'./luigi-core',from:{glob:'**',dot:true}},
-          {context:'node_modules/@luigi-project/client',to:'./luigi-client',from:{glob:'**',dot:true}},
           // {
           //   from: 'node_modules/fundamental-styles/dist',
           //   to: './fundamental-styles'
-          // },
-          // {
-          //   from: 'node_modules/@sap-theming/theming-base-content',
-          //   to: './fonts'
           // },
           {
             from: 'node_modules/@weni/unnnic-system/dist/img',
