3f3dcd29f5cbbb59c4aaae3ff910154312f985f5
Matheus Cristian <mathues@email.com>
2021-09-23 18:43:31 -0300

    feat: added date picker filter and sort filter in billing

--- src/views/billing/billing.vue
+++ src/views/billing/billing.vue
@@ -184,6 +184,56 @@
                 />
               </template>
 
+              <template v-slot:lastEvent>
+                <div :style="{ display: 'flex', alignItems: 'center' }">
+                  <div class="break-text" :style="{ flex: 1 }">
+                    {{ $t('billing.invoices.last_event') }}
+                  </div>
+
+                  <unnnic-icon-svg
+                    size="xs"
+                    :icon="`sort-${sorts.lastEvent}`"
+                    :scheme="
+                      sorts.lastEvent === 'default'
+                        ? 'neutral-clean'
+                        : 'brand-weni-soft'
+                    "
+                    clickable
+                    @click="
+                      sorts.lastEvent =
+                        sorts.lastEvent === 'default'
+                          ? 'asc'
+                          : sorts.lastEvent === 'asc'
+                          ? 'desc'
+                          : 'default'
+                    "
+                  />
+
+                  <span :class="['dropdown', { active: showCalendarFilter }]">
+                    <unnnic-icon-svg
+                      size="xs"
+                      icon="filter"
+                      :scheme="
+                        showCalendarFilter ? 'brand-weni-soft' : 'neutral-clean'
+                      "
+                      clickable
+                      @click="showCalendarFilter = !showCalendarFilter"
+                    />
+
+                    <div class="dropdown-data">
+                      <unnnic-date-picker
+                        clearLabel="Limpar"
+                        actionLabel="Filtrar"
+                        :months="months"
+                        :days="days"
+                        :options="options"
+                        @submit="changeDate"
+                      />
+                    </div>
+                  </span>
+                </div>
+              </template>
+
               <template v-slot:view>
                 <div class="action">
                   {{ $t('billing.invoices.view') }}
@@ -435,7 +485,71 @@ export default {
       ],
 
       loading: false,
+
+      showCalendarFilter: false,
+
+      months: [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro',
+      ],
+
+      days: ['D', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+
+      options: [
+        {
+          name: 'Últimos 7 dias',
+          id: 'last-7-days',
+        },
+        {
+          name: 'Últimos 14 dias',
+          id: 'last-14-days',
+        },
+        {
+          name: 'Últimos 30 dias',
+          id: 'last-30-days',
+        },
+        {
+          name: 'Últimos 12 meses',
+          id: 'last-12-months',
+        },
+        {
+          name: 'Mês Atual',
+          id: 'current-month',
+        },
+        {
+          name: 'Personalizar',
+          id: 'custom',
+        },
+      ],
+
+      sorts: {
+        lastEvent: 'default',
+        payment: 'default',
+        paymentStatus: 'default',
+        contacts: 'default',
+        value: 'default',
+      },
     };
+  },
+
+  created() {
+    window.addEventListener('click', (event) => {
+      if (event.target.closest('.dropdown')) {
+        return false;
+      }
+
+      this.showCalendarFilter = false;
+    });
   },
 
   computed: {
@@ -540,6 +654,10 @@ export default {
         selected: value,
       }));
     },
+
+    changeDate(value) {
+      console.log('value', value);
+    },
   },
 };
 </script>
@@ -620,6 +738,23 @@ export default {
 
       .action {
         text-align: center;
+      }
+
+      .dropdown {
+        position: relative;
+
+        .dropdown-data {
+          position: absolute;
+          pointer-events: none;
+          display: none;
+          left: 100%;
+          top: 100%;
+        }
+
+        &.active .dropdown-data {
+          pointer-events: auto;
+          display: block;
+        }
       }
     }
 
