48a8942d38cf570baf671bac7958935220f2e23d
Matheus Martins Sarmento <matheusms280100@gmail.com>
2021-12-10 17:33:50 -0300

    fix(form) remove unecessary code

--- src/components/billing/FormCreditCard.vue
+++ src/components/billing/FormCreditCard.vue
@@ -93,11 +93,6 @@ export default {
     },
   },
 
-  mounted(){
-    this.$store.state.BillingSteps.billing_details.cpfOrCnpj = null
-    this.$store.state.BillingSteps.billing_details.name = null
-  },
-
   methods: {
     ...mapActions(['nextBillingStep', 'setBillingStep', 'setBillingStep']),
 
