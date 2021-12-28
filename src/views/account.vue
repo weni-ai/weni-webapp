38ea748978ea51c9a087a0b597d13075126344d4
Matheus Cristian <mathues@email.com>
2021-12-28 14:52:48 -0300

    feat: added phone format

--- src/views/account.vue
+++ src/views/account.vue
@@ -81,11 +81,11 @@
           <unnnic-input
             v-model="contact"
             icon-left="phone-3"
+            ref="phoneNumber"
             :placeholder="$t('account.contact_placeholder')"
             :label="$t('account.fields.contact')"
             :type="errorFor('contact') ? 'error' : 'normal'"
             :message="errorFor('contact')"
-            mask="+## (##) # ####-####"
           />
         </div>
         <div class="weni-account__field__group">
@@ -206,6 +206,65 @@ export default {
   created() {
     this.getProfile();
   },
+
+  mounted() {
+    const phoneNumber = this.$refs.phoneNumber.$el.querySelector('input');
+
+    phoneNumber.addEventListener('input', () => {
+      const oldValue = phoneNumber.value.split('');
+
+      oldValue.splice(phoneNumber.selectionStart, 0, 'pointer');
+
+      const newValue = oldValue.filter(
+        (char) => char === 'pointer' || /\d/.test(char),
+      );
+
+      if (newValue.length > 1) {
+        newValue.splice(0, 0, '+');
+      }
+
+      const withoutPointer = () => {
+        return newValue.filter((char) => char !== 'pointer').join('');
+      };
+
+      const add = (position, char) => {
+        const hasPointerBefore = newValue
+          .slice(0, position)
+          .includes('pointer');
+        const positionToAdd = hasPointerBefore ? position + 1 : position;
+        newValue.splice(positionToAdd, 0, char);
+      };
+
+      if (/\+55\d/.test(withoutPointer())) {
+        add(3, ' ');
+
+        if (/\+55 \d{3}/.test(withoutPointer())) {
+          add(6, ' ');
+        }
+
+        if (/\+55 \d{2} \d{9}/.test(withoutPointer())) {
+          add(12, '-');
+        } else if (/\+55 \d{2} \d{5}/.test(withoutPointer())) {
+          add(11, '-');
+        }
+      }
+
+      phoneNumber.value = this.contact = newValue
+        .filter((char) => char !== 'pointer')
+        .join('');
+
+      const pointerIndex = newValue.indexOf('pointer');
+
+      if (pointerIndex >= 1 && !/\d/.test(newValue[pointerIndex - 1])) {
+        phoneNumber.selectionStart = phoneNumber.selectionEnd =
+          newValue.indexOf('pointer') - 1;
+      } else {
+        phoneNumber.selectionStart = phoneNumber.selectionEnd =
+          newValue.indexOf('pointer');
+      }
+    });
+  },
+
   methods: {
     ...mapActions([
       'updateProfile',
@@ -263,7 +322,7 @@ export default {
       }
 
       if (key === 'contact') {
-        if (!this.rules.contact.test(this.contact)) {
+        if (!this.contact.length) {
           return this.$t('errors.invalid_contact');
         }
       }
