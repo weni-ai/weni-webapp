b9ab520ed9023063a8feb8a8452f6eb5c302ef81
Matheus Martins Sarmento <matheusms280100@gmail.com>
2022-03-17 18:54:30 -0300

    feat: createOrg test

--- tests/unit/__mocks__/index.js
+++ tests/unit/__mocks__/index.js
@@ -1,9 +1,11 @@
 import org from './org';
 import project from './project';
 import profile from './profile';
+import user from './user';
 
 module.exports = {
   org,
   project,
   profile,
+  user,
 };
---
+++ tests/unit/__mocks__/user.js
@@ -0,0 +1,9 @@
+module.exports = {
+  id: '123',
+  uuid: '321',
+  name: 'Matheus',
+  email: 'm@mr.com',
+  photo: 'photo',
+  role: 'admin',
+  username: 'mathm',
+};
