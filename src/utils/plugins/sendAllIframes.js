65cfe60dda543c169635ba4afa830321b6e257ba
Matheus Cristian <mathues@email.com>
2022-07-07 15:43:44 -0300

    feat: changed sendAllIframes to return amount of sents messages

--- src/utils/plugins/sendAllIframes.js
+++ src/utils/plugins/sendAllIframes.js
@@ -1,4 +1,6 @@
 export default function (event, data) {
+  let sents = 0;
+
   document.querySelectorAll('iframe').forEach(({ contentWindow }) => {
     if (contentWindow && contentWindow.postMessage) {
       contentWindow.postMessage(
@@ -8,6 +10,10 @@ export default function (event, data) {
         },
         '*',
       );
+
+      sents++;
     }
   });
+
+  return sents;
 }
