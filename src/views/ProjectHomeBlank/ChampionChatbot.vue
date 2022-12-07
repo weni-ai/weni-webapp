6df42f421a8f2287af4e267f74f3cfa67bfd7bb4
Matheus Cristian <mathues@email.com>
2022-12-07 09:02:24 -0300

    feat: added champion chatbot integration

--- src/views/ProjectHomeBlank/ChampionChatbot.vue
+++ src/views/ProjectHomeBlank/ChampionChatbot.vue
@@ -5,86 +5,125 @@
     }"
     class="champion-chatbot"
   >
-    <div class="title">
-      {{ $t('home.champion_chatbot.title') }}
+    <template v-if="loading">
+      <unnnic-skeleton-loading height="22px" width="50%" />
+      <unnnic-skeleton-loading height="24px" width="100%" />
+      <unnnic-skeleton-loading height="28px" width="50%" />
+    </template>
 
-      <unnnic-tool-tip
-        :text="$t('home.champion_chatbot.info')"
-        enabled
-        side="right"
-        maxWidth="14rem"
-        class="tooltip"
-      >
-        <unnnic-icon-svg
-          icon="information-circle-4"
-          size="sm"
-          scheme="neutral-clean"
-        />
-      </unnnic-tool-tip>
-    </div>
+    <template v-else>
+      <div class="title">
+        {{ $t('home.champion_chatbot.title') }}
 
-    <div class="form-control">
-      <div class="icon-container color-brand-weni-soft">
-        <unnnic-icon icon="check-double" size="sm" scheme="brand-weni-soft" />
-      </div>
-
-      <template v-for="i in 4">
         <unnnic-tool-tip
-          :text="infosForLabel[i]"
+          :text="$t('home.champion_chatbot.info')"
           enabled
-          side="top"
-          :key="`level-${i}`"
+          side="right"
+          maxWidth="14rem"
+          class="tooltip"
         >
-          <div
-            :class="[
-              'level',
-              {
-                'color-brand-weni-soft': i < level,
-                'color-brand-weni-dark': level === i,
-              },
-            ]"
-          ></div>
+          <unnnic-icon-svg
+            icon="information-circle-4"
+            size="sm"
+            scheme="neutral-clean"
+          />
         </unnnic-tool-tip>
-
-        <div
-          v-if="level === i && level !== 4"
-          class="icon-container color-brand-weni-dark"
-          :key="`icon-${i}`"
-        >
-          <unnnic-icon icon="check-double" size="sm" scheme="brand-weni-dark" />
-        </div>
-      </template>
-
-      <div class="icon-container">
-        <unnnic-icon
-          icon="rating-star-1-1"
-          size="sm"
-          scheme="feedback-yellow"
-        />
       </div>
-    </div>
 
-    <div
-      class="description"
-      v-html="
-        $t('home.champion_chatbot.profile', { profile: 'Chatbot campeão' })
-      "
-    ></div>
+      <div class="form-control">
+        <div class="icon-container color-brand-weni-soft">
+          <unnnic-icon icon="check-double" size="sm" scheme="brand-weni-soft" />
+        </div>
+
+        <template v-for="i in 4">
+          <unnnic-tool-tip
+            :text="infosForLabel[i]"
+            enabled
+            side="top"
+            :key="`level-${i}`"
+          >
+            <div
+              :class="[
+                'level',
+                {
+                  'color-brand-weni-soft': i < level,
+                  'color-brand-weni-dark': level === i,
+                },
+              ]"
+            ></div>
+          </unnnic-tool-tip>
+
+          <div
+            v-if="level === i && level !== 4"
+            class="icon-container color-brand-weni-dark"
+            :key="`icon-${i}`"
+          >
+            <unnnic-icon
+              icon="check-double"
+              size="sm"
+              scheme="brand-weni-dark"
+            />
+          </div>
+        </template>
+
+        <div class="icon-container">
+          <unnnic-icon
+            icon="rating-star-1-1"
+            size="sm"
+            scheme="feedback-yellow"
+          />
+        </div>
+      </div>
+
+      <div
+        class="description"
+        v-html="
+          $t('home.champion_chatbot.profile', { profile: 'Chatbot campeão' })
+        "
+      ></div>
+    </template>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import getEnv from '../../utils/env';
+
 export default {
   data() {
     return {
       level: 0,
+      loading: true,
     };
   },
 
-  mounted() {
-    setInterval(() => {
-      this.level = (this.level + 1) % 5;
-    }, 1000);
+  async created() {
+    try {
+      this.loading = true;
+
+      const response = await axios.get(
+        `${getEnv('URL_FLOWS')}/api/v2/success_orgs.json`,
+        {
+          headers: {
+            Authorization: `Token ${getEnv('FLOWS_GENERIC_TOKEN')}`,
+          },
+          params: {
+            email: this.$store.state.Account.profile.email,
+          },
+        },
+      );
+
+      const { has_ia, has_flows, has_channel, has_msg } =
+        response.data.orgs.find(
+          ({ uuid }) =>
+            uuid === this.$store.getters.currentProject.flow_organization,
+        ) || {};
+
+      this.level =
+        [has_flows, has_ia, has_channel, has_msg].lastIndexOf(true) + 1;
+    } finally {
+      this.loading = false;
+    }
   },
 
   computed: {
