68adb22cd8e23849ddd2f10494facf6955f12f4b
Aldemylla <aldemylla@gmail.com>
2024-08-05 16:37:30 -0300

    fix: Vue test utils config at vite

--- vite.config.js
+++ vite.config.js
@@ -26,11 +26,4 @@ export default defineConfig({
       },
     },
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-    },
-  },
 });
