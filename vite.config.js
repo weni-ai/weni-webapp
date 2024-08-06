d51589e76d7afd57a96adb0e8cd2d628be532e5f
Aldemylla <aldemylla@gmail.com>
2024-08-06 10:48:13 -0300

    fix: PdfMake

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue2';
+import svgLoader from 'vite-svg-loader';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
