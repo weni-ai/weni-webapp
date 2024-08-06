7f646e8b9a147897983f7eb4f1f3c918a3aa9e32
Aldemylla <aldemylla@gmail.com>
2024-08-06 11:11:49 -0300

    fix: Emoji component import emoji file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader({ defaultImport: 'url' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
