e676d250f289076ef1e8af99becc21a5c8ac8a61
Matheus Cristian <mathues@email.com>
2024-08-21 18:42:08 -0300

    feat(redesign-topbar): adds learning center right sidebar

---
+++ src/components/common/RightBar/LearningCenterChampionChatbotYoutubePreview.vue
@@ -0,0 +1,37 @@
+<template>
+  <section
+    class="youtube-preview"
+    :style="{
+      backgroundImage: `url('https://i.ytimg.com/vi_webp/${videoId}/maxresdefault.webp')`,
+    }"
+  >
+    <img
+      class="youtube-preview__play-button"
+      src="@/assets/youtube-play-button.svg"
+    />
+  </section>
+</template>
+
+<script setup>
+defineProps({
+  videoId: String,
+});
+</script>
+
+<style lang="scss" scoped>
+.youtube-preview {
+  aspect-ratio: 16 / 9;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  background-position: center center;
+  border-radius: $unnnic-border-radius-sm;
+
+  &__play-button {
+    pointer-events: none;
+    user-select: none;
+    width: 10%;
+  }
+}
+</style>
