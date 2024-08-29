b6b75bf2d15780df8c2ba73940d5a02d7405d146
Eduardo Medeiros <eduardomedeirosrn2@gmail.com>
2024-08-29 14:24:12 -0300

    feat: change settings sidebar

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,7 @@ export JSON_STRING='window.configs = { \
   "NEXUS_API":"'${NEXUS_API}'", \
   "VITE_HOTJAR_ID":"'${VITE_HOTJAR_ID}'", \
   "VITE_BILLING_API_URL":"'${VITE_BILLING_API_URL}'", \
+  "VITE_CHATS_API_URL":"'${VITE_CHATS_API_URL}'", \
 }'
 sed "s|\/\/CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/connect/index.html.tmpl > /tmp/index.html
 
