6d114086b6f283316e352b7316ce1ec2be844235
Matheus Cristian <mathues@email.com>
2024-09-03 10:38:46 -0300

    feat(new-user-flow): shows modal of created project for new users

--- src/views/register/ModalCreateProjectSuccess.vue
+++ src/views/register/ModalCreateProjectSuccess.vue
@@ -33,6 +33,8 @@ import { computed, getCurrentInstance } from 'vue';
 import AmazoninhaStarryEyes from '@/assets/amazoninha-starry-eyes.png';
 import AmazoninhaThumbsUp from '@/assets/amazoninha-thumbs-up.png';
 
+const emit = defineEmits('close');
+
 const instance = getCurrentInstance();
 
 const props = defineProps({
@@ -51,6 +53,8 @@ const image = computed(() => {
 
 function redirectToTheProject() {
   const router = instance.proxy['$router'];
+
+  emit('close');
 
   if (props.createdBrain) {
     router.push({
