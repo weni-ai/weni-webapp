f8de1cb07bea8931f89c064a6a74af27d1fbb8f1
Marcus Vinícius <marcusvinici25@gmail.com>
2024-10-01 15:32:36 -0300

    fix: render recent activities

--- src/store/project/index.js
+++ src/store/project/index.js
@@ -3,7 +3,6 @@ import projects from '../../api/projects';
 import actions from './actions';
 import mutations from './mutations';
 
-const recentActivities = {};
 const championChatbots = {};
 
 function isUuid(value) {
@@ -27,59 +26,7 @@ const state = {
   },
   championChatbots: {},
   projects: [],
-  recentActivities: new Proxy(recentActivities, {
-    get(target, name, receiver) {
-      if (!Reflect.has(target, name)) {
-        const recentActivities = reactive({
-          status: null,
-          next: null,
-          data: [],
-        });
-
-        recentActivities.loadNext = async () => {
-          recentActivities.status = 'loading';
-
-          await projects
-            .latestActivities({
-              projectUuid: name,
-              limit: 20,
-              next: recentActivities.next,
-            })
-            .then(({ data }) => {
-              if (data.next) {
-                const url = new URL(data.next);
-                const cursor = url.searchParams.get('cursor');
-                recentActivities.next = cursor;
-              }
-
-              const results = [...recentActivities.data, ...data.results];
-
-              recentActivities.data = results.filter(
-                (value, index, self) =>
-                  index ===
-                  self.findIndex(
-                    (item) =>
-                      item.user === value.user &&
-                      item.created_at === value.created_at,
-                  ),
-              );
-
-              recentActivities.status = data.next == null ? 'complete' : null;
-            })
-            .catch(() => {
-              if (recentActivities.status !== 'complete') {
-                recentActivities.status = 'error';
-              }
-            });
-        };
-
-        Reflect.set(target, name, recentActivities, receiver);
-      }
-
-      return Reflect.get(target, name, receiver);
-    },
-  }),
-
+  recentActivities: reactive({}),
   championChatbots2: new Proxy(championChatbots, {
     get(_target, uuid) {
       if (!isUuid(uuid)) {
