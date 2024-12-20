df2722244571f22b0ca7fd4ca27782e000254815
Marcus Vinícius <marcusvinici25@gmail.com>
2024-12-19 23:38:26 -0300

    adding initial logic to auth module federation

---
+++ src/components/Prototype.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const RemoteMFE = defineAsyncComponent(() => import('remote/solution-card'));
+
+const props = defineProps({
+  auth: {
+    type: Object || null,
+    default: null,
+  },
+});
+
+</script>
+
+<template>
+  <RemoteMFE 
+    v-if="!!RemoteMFE && props.auth.token && props.auth.uuid"        
+    type="remote"
+    :auth="{
+      token: `Bearer ${props.auth.token}`,
+      uuid: props.auth.uuid,
+    }"
+  />
+</template>
+
+<style scoped>
+</style>
