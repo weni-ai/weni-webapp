433e82404c90bfff1def916b09c8d962d0d0dcaf
Marcus Vinícius <marcusvinici25@gmail.com>
2025-01-23 09:56:24 -0300

    commet discovery commerce

--- src/components/RemoteComponents.vue
+++ src/components/RemoteComponents.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { defineAsyncComponent } from 'vue';
 
-const RemoteDiscoveryCommerce = defineAsyncComponent(() => import('remote/solution-card'));
+/*const RemoteDiscoveryCommerce = defineAsyncComponent(() => import('remote/solution-card'));
+<RemoteDiscoveryCommerce
+    v-if="!!RemoteDiscoveryCommerce && props.auth.token && props.auth.uuid"
+    type="remote"
+    :auth="{
+      token: `Bearer ${props.auth.token}`,
+      uuid: props.auth.uuid,
+    }"
+/>*/
 const RemoteDashboardInsights = defineAsyncComponent(() => import('remote_insights/dashboard-commerce'));
   
 const props = defineProps({
@@ -14,14 +22,6 @@ const props = defineProps({
 
 <template>
 <RemoteDashboardInsights v-if="!!RemoteDashboardInsights" />
-<RemoteDiscoveryCommerce
-    v-if="!!RemoteDiscoveryCommerce && props.auth.token && props.auth.uuid"
-    type="remote"
-    :auth="{
-      token: `Bearer ${props.auth.token}`,
-      uuid: props.auth.uuid,
-    }"
-/>
 </template>
 
 <style scoped></style>
