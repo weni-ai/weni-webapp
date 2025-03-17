7358f47d52f49a23a83fa3f538d2c74cfa9ad2e8
Marcus Vinícius <marcusvinici25@gmail.com>
2025-03-17 17:51:13 -0300

    adding debug

--- src/components/ExternalSystem.vue
+++ src/components/ExternalSystem.vue
@@ -544,8 +544,8 @@ export default {
       const apiUrl = this.urls.flows;
       if (!apiUrl) return null;
 
-      const baseUrl = `${apiUrl}weni/${uuid || this.$route.params.projectUuid}/authenticate`;
-
+      const baseUrl = `${apiUrl}weni/${this.$route.params.projectUuid || uuid}/authenticate`;
+      console.log('debug ===========>', this.$route.params.projectUuid, uuid);
       return `${baseUrl}${next}${next ? '&' : '?'}access_token=${accessToken}`;
     },
 
