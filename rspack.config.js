77ae23528e6a019f4548e5e5213474ffc31892b3
Marcus Vinícius <marcusvinici25@gmail.com>
2025-03-19 14:59:59 -0300

    adjusting rspack vars

--- rspack.config.js
+++ rspack.config.js
@@ -84,8 +84,8 @@ module.exports = defineConfig({
     new rspack.container.ModuleFederationPlugin({
       name: 'host',
       remotes: {
-        remote: `remote@${process.env.MODULE_FEDERATION_REMOTE_URL || 'https://commerce-webapp.stg.cloud.weni.ai'}/remote.js`,
-        remote_insights: `remote_insights@${process.env.MODULE_FEDERATION_INSIGHTS_URL || 'https://insights.stg.cloud.weni.ai'}/remoteEntry.js`,
+        remote: `remote@${'https://commerce-webapp.stg.cloud.weni.ai'}/remote.js`,
+        remote_insights: `remote_insights@${'https://insights.stg.cloud.weni.ai'}/remoteEntry.js`,
       },
       exposes: {},
       shared: {
