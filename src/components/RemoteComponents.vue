35c7b16f860945e8ad30ba2143b517cc6b6e6c79
Marcus Vinícius <marcusvinici25@gmail.com>
2025-03-19 16:36:06 -0300

    adding auth variables to remoteInsights

--- src/components/RemoteComponents.vue
+++ src/components/RemoteComponents.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { defineAsyncComponent } from 'vue';
 
-const RemoteDiscoveryCommerce = defineAsyncComponent(() => import('remote/solution-card'));
-const RemoteDashboardInsights = defineAsyncComponent(() => import('remote_insights/dashboard-commerce'));
-  
+const RemoteDiscoveryCommerce = defineAsyncComponent(
+  () => import('remote/solution-card'),
+);
+const RemoteDashboardInsights = defineAsyncComponent(
+  () => import('remote_insights/dashboard-commerce'),
+);
+
 const props = defineProps({
   auth: {
     type: Object || null,
@@ -13,15 +17,20 @@ const props = defineProps({
 </script>
 
 <template>
-<RemoteDashboardInsights v-if="!!RemoteDashboardInsights" />
-<RemoteDiscoveryCommerce
+  <RemoteDashboardInsights
+    v-if="!!RemoteDashboardInsights && props.auth.token && props.auth.uuid"
+    type="remote"
+    :auth="{
+      token: `Bearer ${props.auth.token}`,
+      uuid: props.auth.uuid,
+    }"
+  />
+  <RemoteDiscoveryCommerce
     v-if="!!RemoteDiscoveryCommerce && props.auth.token && props.auth.uuid"
     type="remote"
     :auth="{
       token: `Bearer ${props.auth.token}`,
       uuid: props.auth.uuid,
     }"
-/>
+  />
 </template>
-
-<style scoped></style>
