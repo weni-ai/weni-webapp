af4301a2f8ac1f8b7c5c2435e45ae314778252f6
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-03-26 16:25:43 -0300

    fix: reorder environment variable retrieval logic in getEnv function

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,8 +2,7 @@ import YAML from 'yaml';
 
 export default function getEnv(name) {
   const value =
-    process.env?.[name] || window?.configs?.[name] ||
-    import.meta.env[name]
+    process.env?.[name] || import.meta.env[name] || window?.configs?.[name];
 
   if (/(^|_)yaml($|_)/i.test(name)) {
     return YAML.parse(value);
