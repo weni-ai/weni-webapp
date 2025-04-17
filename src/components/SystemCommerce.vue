423d97364f1d29a9a37b7fadba646166f0c38b85
acnormun <anaclara.noronha@weni.ai>
2025-04-17 17:10:45 -0300

    fix: append 'login' to baseSrc URL in SystemCommerce component

--- src/components/SystemCommerce.vue
+++ src/components/SystemCommerce.vue
@@ -144,7 +144,7 @@ export default {
 
         if (!apiUrl) return null;
 
-        this.baseSrc = `${apiUrl}`;
+        this.baseSrc = `${apiUrl}login`;
 
         this.reload();
       } catch (e) {
