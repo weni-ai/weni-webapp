4beb1f8dfccf42f809f06bb9dc7bed2b90ebce7b
Paulo Bernardo <paulo.bernardo@weni.ai>
2025-09-19 15:32:26 -0300

    refactor: simplify hasBulkSendPermission computation in Sidebar component

--- tests/unit/unit/components/Sidebar/Sidebar.spec.js
+++ tests/unit/unit/components/Sidebar/Sidebar.spec.js
@@ -102,11 +102,7 @@ const router = createRouter({
     {
       path: '/projects/1234/integrations',
       component: { template: '<section></section>' },
-    },
-    {
-      path: '/projects/1234/bulkSend',
-      component: { template: '<section></section>' },
-    },
+    }
   ],
 });
 
@@ -187,10 +183,10 @@ describe('Sidebar.vue', () => {
       wrapper = setup();
     });
 
-    it('should show all the sidebar options (13)', () => {
+    it('should show all the sidebar options (12)', () => {
       const sidebarOptions = wrapper.findAllComponents(elements.sidebarOption);
 
-      expect(sidebarOptions.length).toBe(13);
+      expect(sidebarOptions.length).toBe(12);
     });
   });
 
