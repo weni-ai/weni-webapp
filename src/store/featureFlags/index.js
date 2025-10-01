01f4f567b38a212d55d1680f8b065c35b8fcf059
Marcus Vinícius <marcusvinici25@gmail.com>
2025-10-01 18:34:50 -0300

    refactor: remove unused feature flag 'insightsMF' from feature flags store

--- src/store/featureFlags/index.js
+++ src/store/featureFlags/index.js
@@ -25,7 +25,6 @@ export const useFeatureFlagsStore = defineStore('FeatureFlags', () => {
   const flags = computed(() => ({
     agentsTeam: agentsTeam.value,
     newConnectPlataform: growthbook?.isOn('connect-plataform-1.5'),
-    insightsMF: growthbook?.isOn('insights-mf'),
   }));
 
   async function checkAgentBuilder2(projectUuid) {
