18ed69adaa844760223ca8f1c0b0fe393a580e94
Paulo Bernardo <paulo.bernardo@weni.ai>
2026-02-12 21:24:15 -0300

    feat: use new webchat script

--- src/utils/plugins/WebChat.js
+++ src/utils/plugins/WebChat.js
@@ -1,63 +1,32 @@
 import { waitFor } from '../waitFor';
 import { transformIntoDraggableBubble } from '../transformIntoDraggableBubble';
 
-export default function (d = document, s = 'script') {
-  const nonce = 'wwc';
+const WEBCHAT_SCRIPT_URL =
+  'https://weni-sp-integrations-production.s3.amazonaws.com/apptypes/wwc/2fd0fbb9-baee-42e4-8003-28c2cc90c374/script.js';
 
+export default function (
+  d = document,
+  s = 'script',
+  u = WEBCHAT_SCRIPT_URL,
+  w = window,
+  v = 'isWeniWebChatAlreadyInserted',
+) {
+  const nonce = 'wwc';
+  if (w[v]) {
+    return;
+  } else {
+    w[v] = !0;
+  }
   let h = d.getElementsByTagName(s)[0],
     k = d.createElement(s);
-  k.setAttribute('nonce', nonce);
-
   k.onload = function () {
-    let j = d.createElement('div');
-    j.id = 'wwc';
-    d.body.appendChild(j);
-
-    // WebChat Config
-    let p = {
-      title: 'Especialista',
-      inputTextFieldHint: 'Digite aqui sua mensagem...',
-      showFullScreenButton: true,
-      displayUnreadCount: true,
-      initPayload: 'Falar com especialista',
-      mainColor: '#009E96',
-      profileAvatar:
-        'https://weni-sp-integrations-production.s3.amazonaws.com/apptypes/wwc/1807661c-caf2-4a13-9c64-b2006c4f15dd/avatar.png',
-      tooltipMessage: 'Oi! Tem um especialista pronto para te ajudar.',
-      openLauncherImage:
-        'https://weni-sp-integrations-production.s3.amazonaws.com/apptypes/wwc/1807661c-caf2-4a13-9c64-b2006c4f15dd/avatar.png',
-      selector: '#wwc',
-      customizeWidget: {
-        headerBackgroundColor: '#009E96',
-        launcherColor: '#009E96',
-        userMessageBubbleColor: '#009E96',
-        quickRepliesFontColor: '#009E96',
-        quickRepliesBackgroundColor: '#009E9633',
-        quickRepliesBorderColor: '#009E96',
-      },
-      params: {
-        images: {
-          dims: {
-            width: 300,
-            height: 200,
-          },
-        },
-        storage: 'session',
-      },
-      socketUrl: 'https://websocket.weni.ai',
-      host: 'https://flows.weni.ai',
-      channelUuid: 'a326544e-7059-4ff1-b397-bb6ad6a5167f',
-    };
-
-    p['customMessageDelay'] = () => {
-      return 1 * 1000;
-    };
-
-    WebChat.default.init(p);
+    let l = d.createElement(s);
+    l.src = u;
+    l.async = true;
+    h.parentNode.insertBefore(l, k.nextSibling);
   };
-
   k.async = true;
-  k.src = 'https://storage.googleapis.com/push-webchat/wwc-2.19.1.js';
+  k.src = 'https://cdn.cloud.weni.ai/webchat-latest.umd.js';
   h.parentNode.insertBefore(k, h);
 
   waitFor(() => document.querySelector('button.push-launcher')).then((button) =>
