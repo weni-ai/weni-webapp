b49e2c1d36dea24343020cb7aeca7633bcaaacfd
Aldemylla <aldemylla@gmail.com>
2026-08-05 18:57:58 -0300

    fix: update query handling in app.vue to merge payload query with existing query

--- src/app.vue
+++ src/app.vue
@@ -727,7 +727,7 @@ export default {
             projectUuid: this.$route.params.projectUuid,
             internal,
           },
-          query,
+          query: { ...query, ...(payload?.query || {}) },
         };
 
         if (payload?.openInNew) {
