c3a02169a1aefbeae628e042a2fbd85c0ac947ef
Aldemylla <aldemylla@gmail.com>
2026-08-06 11:33:27 -0300

    feat: add change history feature in settings
    - Introduced a new route for change history in settings with authentication.
    - Added localization support for change history in English, Spanish, Portuguese, and Romanian.
    - Created a new component for displaying change history and integrated it into the settings view.
    - Removed the history of changes tab from the workspace settings.

---
+++ src/views/settings/SettingsChangeHistory.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="settings-change-history">
+    <UnnnicPageHeader
+      :title="$t('settings.change_history.title')"
+      :description="$t('settings.change_history.description')"
+    >
+      <template #actions>
+        <UnnnicButton
+          :text="$t('settings.change_history.export_changes')"
+          type="secondary"
+        />
+      </template>
+    </UnnnicPageHeader>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.settings-change-history {
+  padding: $unnnic-space-4;
+
+  display: flex;
+  flex-direction: column;
+}
+</style>
