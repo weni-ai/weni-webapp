e24d6667797b9f4485133d317f3dd2964958c00f
Paulo Bernardo <paulo.bernardo@vtex.com>
2026-08-14 13:24:53 -0300

    refactor: remove unused dashboard components and API methods
    - Deleted obsolete components: growth, news, newsletter, status, and their associated tests.
    - Removed unused API methods related to dashboard status and newsletter fetching.
    - Cleaned up localization files by removing entries related to the deleted components.
    - Updated the dashboard API to streamline functionality and improve maintainability.

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -1,19 +1,9 @@
 import request from './request.js';
 
 export default {
-  status(orgId, projectUuid) {
-    return request
-      .$http()
-      .get(`/v1/dashboard/status-service/?project_uuid=${projectUuid}`);
-  },
-
   newsletterList(offset, limit) {
     return request
       .$http()
       .get(`/v1/dashboard/newsletter/?offset=${offset}&limit=${limit}`);
-  },
-
-  newsletter(orgId, id) {
-    return request.$http().get(`/v1/dashboard/newsletter/${id}`);
   },
 };
