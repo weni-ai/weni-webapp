729afcdadbaf326d82408379619a73d83bb3fcdf
Paulo Bernardo <paulo.bernardo@vtex.com>
2026-08-19 12:16:13 -0300

    refactor: remove duplicate imports in app.vue
    Eliminated redundant imports of useThemeStore and useNewsStore from app.vue to streamline the code and improve readability.

--- src/app.vue
+++ src/app.vue
@@ -187,8 +187,6 @@ import { useChatsThemeStore, CHATS_THEME_DARK } from './store/chatsTheme.js';
 import { useThemeStore } from '@/store/theme';
 import { useNewsStore } from '@/store/news';
 import { buildChatsHostRedirectRoute } from '@/utils/normalizeInternalPath';
-import { useThemeStore } from '@/store/theme';
-import { useNewsStore } from '@/store/news';
 
 const CHATS_DARK_ROUTES = new Set(['chats']);
 const CHATS_LIGHT_ROUTES = new Set(['settingsChats']);
